ed6b3992595242f07bf4b40a2999ce1d8e4b97f4
Charlotte <charlottemaryhailey@gmail.com>
2018-11-12 21:24:36 +0800

    added the jquery for the counter

--- main.js
+++ main.js
@@ -201,8 +201,47 @@ $('a[href*="#"]')
     }
   })
 
+  // $("body").mousemove(function(event){
+  //   console.log(event.pageY)
+  //   if(event.pageY < 650){
+  //     $("a[href='down']").attr('href','#what')
+  //   }
+  //
+  //     else if((event.pageY > 651) && (event.pageY < 1025)){
+  //       $("a[href='down']").attr('href','#why')
+  //     }
 
+  var a = 0;
+  $(window).scroll(function() {
 
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $('.counter-value').each(function() {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+            countNum: countTo
+          },
 
+          {
 
-})
+            duration: 1500,
+            easing: 'swing',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+              //alert('finished');
+            }
+
+          });
+      });
+      a = 1;
+    }
+
+  });
+
+  })
